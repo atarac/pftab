99d9179c1102f911fabe01cc29f2c16b36642a96
atarac <atara.c.daisuke@gmail.com>
2023-09-18 11:17:47 +0900

    とりあえず

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { registerBlockType } from '@wordpress/blocks';
 
-import './tab-container/style.scss';
-import editTabContainer from './tab-container/edit';
-import saveTabContainer from './tab-container/save';
-import metadataTabContainer from './tab-container/block.json';
+import './style.scss';
+import Edit from './edit';
+import save from './save';
+import metadata from './block.json';
 
-registerBlockType(metadataTabContainer.name, {
-	edit: editTabContainer,
-	save: saveTabContainer,
-});
+registerBlockType( metadata.name, {
+	edit: Edit,
+	save,
+} );
