64a7eac452ef0c415743b0d216cfdc5f546d469b
atarac <atara.c.daisuke@gmail.com>
2023-09-18 22:59:06 +0900

    リファクタリング

--- src/tab-container/edit.js
+++ src/tab-container/edit.js
@@ -1,58 +1,29 @@
-// import {
-// 	RichText,
-// 	InnerBlocks,
-// 	useBlockProps
-// } from '@wordpress/block-editor';
+import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 
-// import './editor.scss';
-
-// const ALLOWED_BLOCKS = ['create-block/individual-tab-block']
-
-// export default function Edit( { attributes, setAttributes } ) {
-// 	const blockProps = useBlockProps();
-
-// 	return (
-// 		<div {...blockProps}>
-// 			<ul class="tab-menu">
-// 				{Array.from({ length:10 }, (_, i) => i + 1).map((num) =>{
-// 					const menuName = `menu${num}`;
-// 						return (
-// 							<RichText
-// 								key={menuName}
-// 								tagName='li'
-// 								className={`menu-${num}`}
-// 								value={ attributes[menuName] }
-// 								onChange={ (newMenu) =>
-// 									setAttributes( {
-// 										[menuName]: newMenu
-// 									})
-// 								}
-// 							/>
-// 						);
-// 				})}
-// 			</ul>
-// 			<div class="tab-content">
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
-
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
 	const blockProps = useBlockProps();
-
 	const tabs = Array.from({ length: 10 }, (_, i) => i + 1);
 
 	return (
 		<div {...blockProps}>
 			<div className="tab-container">
 				<div className="tab-menu">
-					{tabs.map((tabNumber) => (
-						<div key={`menu-${tabNumber}`} className={`tab-menu-item tab-menu-item-${tabNumber}`}>
-							Menu{tabNumber}
-						</div>
-					))}
+					{tabs.map((tabNumber) => {
+						const menuName = `menu${tabNumber}`;
+						return (
+							<RichText
+								key={menuName}
+								tagName='li'
+								className={menuName}
+								value={attributes[menuName]}
+								onChange={(newMenuName) =>
+									setAttributes({
+										[menuName]: newMenuName,
+									})
+								}
+							/>
+						);
+					})}
 				</div>
 				<div className="tab-content">
 					<InnerBlocks
