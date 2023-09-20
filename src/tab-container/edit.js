19564efd60c33fdc326d018bb72c83747043a530
atarac <atara.c.daisuke@gmail.com>
2023-09-21 08:43:14 +0900

    conflict調整

--- src/tab-container/edit.js
+++ src/tab-container/edit.js
@@ -1,47 +1,6 @@
 import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 
-<<<<<<< HEAD
-// import './editor.scss';
-
-// const ALLOWED_BLOCKS = ['create-block/individual-tab-block']
-
-// export default function Edit( { attributes, setAttributes } ) {
-// 	const blockProps = useBlockProps();
-
-// 	return (
-// 		<div {...blockProps}>
-// 			<ul class="tab-menu">
-// 				{Array.from({ length:10 }, (_, i) => i + 1).map((num) =>{
-// 					const menuName = `menu${num}`;
-// 						return (
-// 							<RichText
-// 								key={menuName}
-// 								tagName='li'
-// 								className={`menu-${num}`}
-// 								value={ attributes[menuName] }
-// 								onChange={ (newMenu) =>
-// 									setAttributes( {
-// 										[menuName]: newMenu
-// 									})
-// 								}
-// 							/>
-// 						);
-// 				})}
-// 			</ul>
-// 			<div class="tab-content">
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
-
-import './editor.scss';
-
-export default function Edit() {
-=======
 export default function Edit({ attributes, setAttributes }) {
->>>>>>> development3
 	const blockProps = useBlockProps();
 	const tabs = Array.from({ length: 10 }, (_, i) => i + 1);
 
